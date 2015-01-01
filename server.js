49dca3def38f8412258d6ddfbb2dbdd08e59cc27
Patrick van der Tuin <patricktuin@planet.nl>
2015-01-01 20:25:17 +0100

    version 0.1

--- server.js
+++ server.js
@@ -10,76 +10,83 @@ mongoose.connect('mongodb://localhost/food');
 // Add headers
 app.use(function (req, res, next) {
 // Website you wish to allow to connect
-res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 // Request methods you wish to allow
-res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 // Request headers you wish to allow
-res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 // Set to true if you need the website to include cookies in the requests sent
 // to the API (e.g. in case you use sessions)
-res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Credentials', true);
 // Pass to next layer of middleware
-next();
+    next();
 });
 
 app.use(bodyParser.json());
 
 
-  // define model for recipes  =================
-  var Recipes = mongoose.model('Recipes', {
-    name : {type: String, required: true},
-    category : String,
+// define model for recipes  =================
+var Recipes = mongoose.model('Recipes', {
+    name: {type: String, required: true},
     description: String,
-  });
+    category: String,
+    ingredients: Array,
+    preparation: String,
+    prework_time: Number,
+    preparation_time: Number,
+    image: String
+});
 
 // get all recipes
-app.get('/', function(req, res) {
-	console.log('GET request');
-	Recipes.find(function(err, recipes){
-		if (err)
-			res.send(err)
-		res.json(recipes);
+app.get('/', function (req, res) {
+    console.log('GET request');
+    Recipes.find(function (err, recipes) {
+        if (err)
+            res.send(err)
+        res.json(recipes);
 
-	});
+    });
 });
 
 // post recipe
-app.post('/', function(req, res) {
-	console.log('POST' + JSON.stringify(req.body))
-	Recipes.create({
-		name: req.body.name,
-		category: req.body.category,
-		description: req.body.description,
-	}, function(err, recipes){
-		if (err)
-			res.send(err);
-	})
-		res.send("recipes");
+app.post('/', function (req, res) {
+    console.log('POST' + JSON.stringify(req.body))
+    Recipes.create({
+            name: req.body.name,
+            category: req.body.category,
+            description: req.body.description,
+            ingredients: req.body.ingredients,
+            preparation: req.body.preparation,
+            prework_time: req.body.prework_time,
+            preparation_time: req.body.preparation_time,
+            image: req.body.image
+        },
+        function (err, recipes) {
+            if (err)
+                res.send(err);
+        })
+    res.send("recipes");
 
 });
 
 // delete recipe
-app.delete('/:recipe_id', function(req, res){
-	Recipes.remove({
-		_id: req.params.recipe_id
-	}, function(err, recipes) {
-		if (err)
-			res.send(err);
-		Recipes.find(function(err, recipes){
-			if (err)
-				res.send(err);
-			res.json(recipes);
-		});
-	});
+app.delete('/:recipe_id', function (req, res) {
+    console.log('delete api ' + req.params.recipe_id);
+    Recipes.remove({
+        _id: req.params.recipe_id
+    }, function (err, recipes) {
+        if (err)
+            res.send(err);
+    });
 });
 
 
 // create webserver
 var server = app.listen(3000, function () {
 
-  var host = server.address().address
-  var port = server.address().port
+    var host = server.address().address
+    var port = server.address().port
 
-  console.log('Example app listening at http://%s:%s', host, port)
+    console.log('Example app listening at http://%s:%s', host, port)
 
 })
